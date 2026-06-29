4749d0e750df7495bbac3d2f397be7345727fac0
rodryv99 <dryv99@gmail.com>
2026-06-29 18:55:56 -0400

    add card images and config

--- apps/mobile/lib/config/api_config.dart
+++ apps/mobile/lib/config/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
-  static const String baseUrl = 'http://localhost:3000/api';
-  static const String socketUrl = 'http://localhost:3000';
+  static const String baseUrl = 'https://coup-digital-backend.onrender.com/api';
+  static const String socketUrl = 'https://coup-digital-backend.onrender.com';
 } 
